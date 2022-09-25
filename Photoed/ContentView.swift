a16895765b1748bbd23f29ed6700dedd2123b956
sypianBS <sypianbs@gmail.com>
2022-09-25 18:01:36 +0200

    create pick photo screen

--- Photoed/ContentView.swift
+++ Photoed/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        //used to divide the screen into 3 equal spaces
+        GeometryReader { geo in
+            VStack {
+                logoView.frame(height: geo.size.height*1/3)
+                
+                pickPhotoView.frame(height: geo.size.height*1/3)
+                
+                exitButtonView.frame(height: geo.size.height*1/3)
+                
+            }.frame(maxWidth: .infinity) //take entire screen width
+        }
+    }
+    
+    var logoView: AnyView {
+        return AnyView(
+            Text("Photoed")
+                .font(.title))
+    }
+    
+    var pickPhotoView: AnyView {
+        return AnyView(VStack {
+            ZStack {
+                Circle()
+                    .stroke(style: StrokeStyle(lineWidth: 3, dash: [3]))
+                    .frame(width: 300, height: 300)
+                Image(systemName: "photo.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+            }
+            
+            Button {
+                print("tapped")
+            } label: {
+                Text("Tap to pick photo")
+            }
+        })
+    }
+    
+    var exitButtonView: AnyView {
+        return AnyView( Button {
+            print("Exit tapped")
+        } label: {
+            Text("Exit")
+                .foregroundColor(.red)
+        })
     }
 }
 
