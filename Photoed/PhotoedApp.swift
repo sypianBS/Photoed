90a9cb454a448020fa072d4e68679abe7e16d54d
sypianBS <sypianbs@gmail.com>
2022-09-25 17:12:31 +0200

    Initial Commit

---
+++ Photoed/PhotoedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoedApp.swift
+//  Photoed
+//
+//  Created by Beniamin on 25.09.22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
