83cb780f05d816613d1546a7b4e3446e288657f0
sypianBS <sypianbs@gmail.com>
2022-09-27 20:08:17 +0200

    add a footer view for photo editing

--- Photoed/EditPhotoDetailView.swift
+++ Photoed/EditPhotoDetailView.swift
@@ -12,15 +12,23 @@ struct EditPhotoDetailView: View {
     var image: UIImage
     
     var body: some View {
-        Image(uiImage: image)
-            .resizable()
-            .aspectRatio(contentMode: self.contentMode)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .clipped()
-            .onTapGesture(count: 2) {
-                withAnimation{
-                    self.contentMode = self.contentMode == .fit ? .fill : .fit
-                }
-            } ///double tap will toggle between the modes
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: self.contentMode)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .clipped()
+                    .onTapGesture(count: 2) {
+                        withAnimation{
+                            self.contentMode = self.contentMode == .fit ? .fill : .fit
+                        }
+                    } //double tap will toggle between the modes
+                EditPhotoFooterView()
+                    .frame(maxWidth: .infinity, minHeight: 80, maxHeight: 100)
+            }
+        }
     }
 }
