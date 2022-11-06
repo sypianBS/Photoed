d779b36d076ed3539b843071c6e652fb15d07dd0
sypianBS <sypianbs@gmail.com>
2022-11-06 18:24:08 +0100

    fix contrast range

--- Photoed/Models/FilterSaturation.swift
+++ Photoed/Models/FilterSaturation.swift
@@ -10,6 +10,6 @@ import Foundation
 //min values source: https://cifilter.io/CIColorControls/
 struct FilterRange {
     public static let saturationRange: ClosedRange<Double> = 0...2
-    public static let contrastRange: ClosedRange<Double> = 0...1
+    public static let contrastRange: ClosedRange<Double> = 0...2
     public static let brightnessRange: ClosedRange<Double> = -1...1
 }
